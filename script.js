c7695ed7b8f453f6ea2d8e845e7774be674a235e
DonYall <highvoltage9886@gmail.com>
2023-12-20 13:20:13 -0500

    ps update

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 function calculate() {
     const drill = document.getElementById("drill").value;
     const tutorial = document.getElementById("tutorial").value;
-    let psList = [];
-    document.querySelectorAll(".p").forEach((p) => {
-        psList.push(p.value);
-    });
-    const topSeven = psList.sort().slice(2, 9);
-    let total = 0;
-    topSeven.forEach((p) => {
-        total += parseInt(p);
-    });
-    const averagePS = total / 7;
+    const ps = document.getElementById("ps").value;
     const test = document.getElementById("test").value;
     const bonus = parseFloat(document.getElementById("bonus").value);
-    const totalScore = (drill * 0.1) + (tutorial * 0.1) + (averagePS * 0.49) + (test * 0.2);
+    const totalScore = (drill * 0.1) + (tutorial * 0.1) + (ps * 0.49) + (test * 0.2);
     let goalScore = document.getElementById("final").value;
     if (goalScore % 5 == 0) {
         goalScore -= 0.5;
